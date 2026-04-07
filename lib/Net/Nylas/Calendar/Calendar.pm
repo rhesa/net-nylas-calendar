0b0a0da9fadda47924e7a37c57dbeeadbd8e017a
rhesa <rhesa@cpan.org>
2026-04-07 13:47:33 +0200

    Add Calendar model

---
+++ lib/Net/Nylas/Calendar/Calendar.pm
@@ -0,0 +1,29 @@
+package Net::Nylas::Calendar::Calendar;
+
+use Moose;
+with 'Net::Nylas::Calendar::ToJson';
+
+has [ qw(
+    id grant_id name description location timezone
+    read_only is_primary object hex_color hex_foreground_color
+) ], is => 'ro';
+
+=pod
+
+{
+  "id": "string",
+  "grant_id": "string",
+  "name": "string",
+  "description": "string",
+  "location": "string",
+  "timezone": "string",
+  "read_only": false,
+  "is_primary": false,
+  "object": "calendar",
+  "hex_color": "#0000FF",
+  "hex_foreground_color": "#FFFFFF"
+}
+
+=cut
+
+1;
